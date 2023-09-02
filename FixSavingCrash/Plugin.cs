f4a848f1e4a868b7a91b8bbdc549c5367901af23
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2023-09-02 20:16:47 +0300

    Add FixSavingCrash

---
+++ FixSavingCrash/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace FixSavingCrash;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        Plugin.Log = base.Log;
+
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+    }
+
+    [HarmonyPatch(typeof(StandaloneAntiCheat), nameof(StandaloneAntiCheat.IsCheatEngineRunning))]
+    [HarmonyPostfix]
+    static void IsCheatEngineRunningPostfix(ref bool __result)
+    {
+        if (__result)
+        {
+            Log.LogWarning($"Cheat Engine is running! Preventing StandaloneAntiCheat from crashing the game.");
+            __result = false;
+        }
+    }
+}
